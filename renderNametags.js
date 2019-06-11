83a49589dfda698610e10dd2bf055219138f5d69
antony tsygankov <antonytsygankov@antonys-MacBook-Pro.local>
2019-06-11 19:59:01 -0400

    added render

--- renderNametags.js
+++ renderNametags.js
@@ -1,11 +1,24 @@
 
 function renderNametags(nametags) {
+    result = nametags.map(element => {
+        return createBox(element);
+    }).join("")
     return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(nametags)}</code>
+            ${result}
         </div>
     `
 }
+
+function createBox(name){
+    return `<div class="name_tag">
+    <div class="header"><h1>Hello, My Name is:</h1></div>
+    <div class="name"><h2>${name}</h2></div>
+    </div>
+    `
+}
+
+
 
 function nametags() {
     var content = document.getElementById('content');
